e3b40ced2561f7e73e6fffa73abe0c69348a3d10
tetuya-azami <zqianjian35@gmail.com>
2024-01-08 21:48:28 +0900

    first commit

---
+++ src/main/kotlin/com/example/dddpractice/DddPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dddpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DddPracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<DddPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/dddpractice/DddPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.dddpractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DddPracticeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
